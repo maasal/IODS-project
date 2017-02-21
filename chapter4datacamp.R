9bab91d1c4c19644abeeba8a9c490a2bb26cc214
Salonen Mikko <miksalo@dx3-katal03.valt.helsinki.fi>
2017-02-21 13:19:00 +0200

    Chapter 5 added

--- chapter4datacamp.R
+++ chapter4datacamp.R
@@ -82,7 +82,7 @@ cor(human_) %>% corrplot
 # Singular Value Decomposition the most important tool
 # SVD decomposes a matrix
 # Correspondence Analysis (CA) uses categorical variables (same as LDA
-# # principal componenst:
+# # principal components:
 # 1st captures the most variability
 # 2nd orthogonal and takes avay left variability
 # This is true for each principal component. They are all uncorrelated and each is less important than the previous one, in terms of captured variance
@@ -90,6 +90,123 @@ cor(human_) %>% corrplot
 # PCA, if PC is for example 70.23, this means 70,23% of the variance is captured by this component (PCA1) for example
 # PCA has no criteria or target variable, unsupervised method (assumes larger variance => more important, STANDARDIZE)
 # PCA is powerful at encapsulating correlations between the original features into a smaller number of uncorrelated dimensions
+
+# modified human is available
+
+# standardize the variables
+human_std <- scale(human)
+
+# print out summaries of the standardized variables
+summary(human_std)
+
+# perform principal component analysis (with the SVD method)
+pca_human <- prcomp(human_std)
+
+# draw a biplot of the principal component representation and the original variables
+biplot(pca_human, choices = 1:2, cex = c(0.4, 0.6), col = c("grey40","deeppink2"))
+
+#BIPLOTS
+# displays: observations in a lower 2- dimensional represation and the original features and their relationships with both each other and the principal components
+# the angle between the arrows representing the original features can be interpret as the correlation between the features. Small angle = high positive correlation.
+# the angle between a feature and a PC axis can be interpret as the correlation between the two. Small angle = high positive correlation
+# the length of the arrows are proportional to the standard deviations of the features
+# biplot vizualises LDA, PCA, CA and multiple CA
+# arrows near = high correlation, 90 degree corner => orthogonal
+# features and principals, the features that are pointing towards the first principal component (PC1) are the ones that contribute to that dimension
+# feature pointing to the second is the feature contributing to that dimension.
+#A biplot is a way of visualizing the connections between two representations of the same data. First, a simple scatter plot is drawn where the observations are represented by two principal components (PC's). Then, arrows are drawn to visualize the connections between the original variables and the PC's. The following connections hold:
+
+#The angle between the arrows can be interpret as the correlation between the variables.
+#The angle between a variable and a PC axis can be interpret as the correlation between the two.
+#The length of the arrows are proportional to the standard deviations of the variables
+
+# pca_human, dplyr are available
+
+# create and print out a summary of pca_human
+s <- summary(pca_human)
+
+s
+# rounded percetanges of variance captured by each PC
+pca_pr <- round(1*s$importance[2, ], digits = 3)*100
+
+# print out the percentages of variance
+pca_pr
+
+# create object pc_lab to be used as axis labels
+pc_lab <- paste0(names(pca_pr), " (", pca_pr, "%)")
+
+# draw a biplot
+biplot(pca_human, cex = c(0.8, 1), col = c("grey40", "deeppink2"), xlab = pc_lab[1], ylab = pc_lab[2])
+
+#Multiple Correspondence Analysis
+#Analyses the pattern of relationships of several categorical variables
+#deals with categorical variables, continuous can be used as background variables
+# uses frequencies
+
+#categorical variables: indicator matrix (binary matrix), burt matrix (two-way-corss tabulations between all variables)
+# eigenvalues: the variance oand the percentage of variances retained by each dimension
+# individuals: the individuals coordinates, the dindividuals contribution (%) on the dimension and the cos2( the squared correlations) on the dimensions
+
+#categories:  the coordinates of the varuable categories, the contribution (%), the cos2, v.test values. The v.test follows normal distribution. if the value is below or above 1.96 the coordinate is significantly different from zero
+#categorical variables: the squared correlation between each variable and the dimensions. If the value is close to one it indicates a strong link with the variable and dimension
+
+#plot.MCA() (FactoMineR)
+# the distance between variable categories gives a measure of their similarity
+# for example Lablel2 and Name2 are more similar than Label2 and Level2 and label3 is very different from all the other categories (farther away)
+
+library(FactoMineR)
+
+# the tea dataset and packages FactoMineR, ggplot2, dplyr and tidyr are available
+
+# column names to keep in the dataset
+keep_columns <- c("Tea", "How", "how", "sugar", "where", "lunch")
+
+# select the 'keep_columns' to create a new dataset
+tea_time <- select(tea, one_of(keep_columns))
+
+# look at the summaries and structure of the data
+summary(tea_time)
+str(tea_time)
+
+# visualize the dataset
+gather(tea_time) %>% ggplot(aes(value)) + facet_wrap("key", scales = "free") + geom_bar() + theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8))
+
+
+# tea_time is available
+
+# multiple correspondence analysis
+mca <- MCA(tea_time, graph = FALSE)
+
+# summary of the model
+summary(mca)
+
+# visualize MCA
+plot(mca, invisible=c("ind"), habillage = "quali")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- data/create_human.R
+++ data/create_human.R
@@ -1,33 +1,110 @@
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = ")..")
 library(dplyr)
+library(stringr)
+library(tidyr)
 
 
-humandev <- rename(hd, HDIRank = HDI.Rank, Country = Country, HDI = Human.Development.Index..HDI. , LifeExpectancy = Life.Expectancy.at.Birth , ExpectedEducation= Expected.Years.of.Education , Education = Mean.Years.of.Education , GNI = Gross.National.Income..GNI..per.Capita , GNI_HDI =GNI.per.Capita.Rank.Minus.HDI.Rank )
+# The data combines several indicators from most countries in the world
+
+#"Country" = Country name
+
+# Health and knowledge
+
+#"GNI" = Gross National Income per capita
+#"Life.Exp" = Life expectancy at birth
+#"Edu.Exp" = Expected years of schooling 
+#"Mat.Mor" = Maternal mortality ratio
+#"Ado.Birth" = Adolescent birth rate
+
+# Empowerment
+
+#"Parli.F" = Percetange of female representatives in parliament
+#"Edu2.F" = Proportion of females with at least secondary education
+#"Edu2.M" = Proportion of males with at least secondary education
+#"Labo.F" = Proportion of females in the labour force
+#"Labo.M" " Proportion of males in the labour force
+
+#"Edu2.FM" = Edu2.F / Edu2.M
+#"Labo.FM" = Labo2.F / Labo2.M
+
+str(hd$Gross.National.Income..GNI..per.Capita)
+
+hd$Gross.National.Income..GNI..per.Capita <- str_replace(hd$Gross.National.Income..GNI..per.Capita, pattern=",", replace ="") %>% as.numeric
+
+humandev <- rename(hd, HDIRank = HDI.Rank,
+                   Country = Country,
+                   HDI = Human.Development.Index..HDI. ,
+                   Life.Exp = Life.Expectancy.at.Birth ,
+                   Edu.Exp = Expected.Years.of.Education ,
+                   Edu = Mean.Years.of.Education ,
+                   GNI = Gross.National.Income..GNI..per.Capita ,
+                   GNI.HDI =GNI.per.Capita.Rank.Minus.HDI.Rank )
 dim(humandev)
 str(humandev)
+head(humandev)
 
-
-gender <- mutate(gii, GIIRank = as.integer(GII.Rank), 
-              GII= as.numeric(Gender.Inequality.Index..GII.), 
-              Mort = as.numeric(Maternal.Mortality.Ratio), 
-              BR = as.numeric(Adolescent.Birth.Rate),
-              Rep = as.numeric(Percent.Representation.in.Parliament),
+gender <- mutate(gii, GIIRank = as.integer(GII.Rank),
+              Country = Country, 
+              GII = as.numeric(Gender.Inequality.Index..GII.), 
+              Mat.Mor = as.numeric(Maternal.Mortality.Ratio), 
+              Ado.Birth = as.numeric(Adolescent.Birth.Rate),
+              Parli.F = as.numeric(Percent.Representation.in.Parliament),
               EduF = as.numeric(Population.with.Secondary.Education..Female.),
               EduM = as.numeric(Population.with.Secondary.Education..Male.),
               LabF = as.numeric(Labour.Force.Participation.Rate..Female.) ,
               LabM = as.numeric(Labour.Force.Participation.Rate..Male.))
-gender <- select(gender, one_of(c("GIIRank","Country","GII","Mort","BR","Rep","EduF","EduM","LabF","LabM")))
-gender <- mutate(gender, LabRatio = LabF / LabM, EduRatio = EduF / EduM)
+gender <- select(gender, one_of(c("GIIRank","Country","GII","Mat.Mor","Ado.Birth","Parli.F","EduF","EduM","LabF","LabM")))
+gender <- mutate(gender, Labo.FM = LabF / LabM, Edu2.FM = EduF / EduM)
 dim(gender)
 str(gender)
+head(gender)
 
-human <- inner_join(human,gender,by= "Country")
+human <- inner_join(humandev,gender,by= "Country")
 dim(human)
 str(human)
 
-write.csv(x = human,file = "human.csv")
-rm(gender,gii,human,humandev,hd)
 
-test_data <- read.csv(file = "human.csv",row.names = "X")
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+head(human)
+
+# print out a completeness indicator of the 'human' data
+comp <- complete.cases(human)
+comp
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = comp)
+
+# filter out all rows with NA values
+human <- filter(human, comp)
+
+# human without NA is available
+
+# look at the last 10 observations of human
+tail(human,10)
+
+# define the last indice we want to keep
+last <- nrow(human) - 7
+
+# choose everything until the last 7 observations, that is, no regions
+human <- human[1:last, ]
+
+# add countries as rownames
+rownames(human) <- human$Country
+human <- select(human, -Country)
+head(human)
+
+#now the data is clean without NA's, regions and unusefull variables
+
+
+write.csv(x = human,file = "data/human.csv")
+rm(gender,gii,human,humandev,hd,comp,keep,last)
+
+test_data <- read.csv(file = "data/human.csv",row.names = "X")
+head(test_data)
 summary(test_data)
+
+#csv-writing worked fine, we can continue with the analysis
